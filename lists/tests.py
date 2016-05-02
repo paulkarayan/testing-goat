c2496e82246861fff6e6fe57a54416a7a658623b
Paul Karayan <paulkarayan@gmail.com>
2016-05-02 15:16:50 -0700

    only save item when necessary

--- lists/tests.py
+++ lists/tests.py
@@ -26,6 +26,10 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
+        self.assertEqual(Item.objects.count(), 1) #objects.all().count()
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
@@ -33,6 +37,10 @@ class HomePageTest(TestCase):
         )
         self.assertEqual(response.content.decode(), expected_html)
 
+    def test_homepage_saves_items_only_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
@@ -52,4 +60,3 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'First list item')
         self.assertEqual(second_saved_item.text, 'second list item')
-
