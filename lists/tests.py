7b029a049546abb412937e5e44ac078759adbbae
Paul Karayan <paulkarayan@gmail.com>
2016-05-02 15:30:34 -0700

    failing func tests but can show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -45,6 +45,16 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_homepage_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_getting_items(self):
