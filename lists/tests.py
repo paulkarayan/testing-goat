eed7867e711b9991e6c68686c64f7412bb530593
Paul Karayan <paulkarayan@gmail.com>
2016-05-17 22:31:30 -0700

    fixed weird flaky tests and remaining list blocking fails

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -26,8 +26,10 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
+        list_ = List.objects.create()
+
+        Item.objects.create(text='item 1', list=list_)
+        Item.objects.create(text='item 2', list=list_)
 
         response = self.client.get('/lists/da-only-list/')
 
@@ -35,16 +37,25 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'item 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_getting_items(self):
+        list_ = List()
+        list_.save()
+
+        #assign items to List object
         first_item = Item()
         first_item.text = 'First list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'second list item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -52,7 +63,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'First list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'second list item')
+        self.assertEqual(second_saved_item.list, list_)
 
 class NewListTest(TestCase):
 
