9f1e3404e2c4965a939aea6df19f03d5b01b84cf
Paul Karayan <paulkarayan@gmail.com>
2016-05-27 15:53:19 -0700

    set STATIC_ROOT in settings and collectstatic

--- superlists/settings.py
+++ superlists/settings.py
@@ -101,3 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+#make relative for collectstatic ease
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
